86e0a4685bb5525f6bf870e521756cf12d73beee
Chris Young and Michelle Brecunier <michelle.brecunier@gmail.com>
2016-04-19 10:10:09 -0700

    load StoryActivity on submitButton click

--- app/src/main/java/com/example/guest/bonkerstories/MainActivity.java
+++ app/src/main/java/com/example/guest/bonkerstories/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.guest.bonkerstories;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mSubmitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSubmitButton = (Button) findViewById(R.id.submitButton);
+            mSubmitButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent = new Intent(MainActivity.this, StoryActivity.class);
+                    startActivity(intent);
+                }
+            });
     }
 }
